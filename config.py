03bb577eefd285232642ce0454f57fa1f24caae3
Aliks Denisov <62519228+DeAliks@users.noreply.github.com>
2025-11-10 14:08:35 +0300

    config.py

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-BOT_TOKEN = "7774897924:AAFToR0T82Cl9uAMSLEKhyE2zigwmmiQ7iU"
+# Для основного бота в тг
+BOT_TOKEN = "Ваш_токен_Бота"
 TIMEZONE = "Europe/Moscow"
+
+# Для телеграм группы
+GROUP_CHAT_ID = "@dark_syndicated"  # Чат/канал для уведомлений
+GROUP_TOPIC_ID = 7  # ID темы (если есть)
+GROUP_GUILD = "DarkSyndicate"  # Гильдия для групповых уведомлений
+
+# Ссылка на таблицу гугл с боссами
+SpreadSheet_URL = "https://docs.google.com/spreadsheets/d/1FkMJd5Oj1MFy4TQZm379PDT3wu0gAqaxdgUgvRr9e-c/edit?gid=275501199#gid=275501199"
