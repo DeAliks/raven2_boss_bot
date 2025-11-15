2d1b70975000f6c1b703a92b989828e0e49b3cf1
DeAliks <de.aliks@gmail.com>
2025-11-15 16:43:38 +0300

    Бан игроков к боту + гитигнор + реадми и прочее

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Для основного бота в тг
-BOT_TOKEN = "7774897924:AAFToR0T82Cl9uAMSLEKhyE2zigwmmiQ7iU"
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
 TIMEZONE = "Europe/Moscow"
 
 # Для телеграм группы
@@ -11,7 +11,8 @@ GROUP_GUILD = "DarkSyndicate"  # Гильдия для групповых уве
 SpreadSheet_URL = "https://docs.google.com/spreadsheets/d/1FkMJd5Oj1MFy4TQZm379PDT3wu0gAqaxdgUgvRr9e-c/edit?gid=275501199#gid=275501199"
 
 #Для бота дс
-DISCORD_TOKEN = "MTQzNzQzMzM2MTA0NzA5NzQyNg.GDjxgU.-ejw3d4aN-wSsfz0p0G79Jrwk3rKDfQFTqeDeQ"
+
+DISCORD_BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Замените на реальный токен
 
 # ID роли Raven2 для упоминаний
 DISCORD_ROLE_ID = "1426622831692877864"
