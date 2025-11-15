644f2c5a333b2708039509ad6c2134b894b9bedf
DeAliks <de.aliks@gmail.com>
2025-11-15 16:12:03 +0300

    Добавлены .gitignore, readme, setup.py и другие файлы

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Для основного бота в тг
-BOT_TOKEN = "Ваш_токен_Бота"
+BOT_TOKEN = "7774897924:AAFToR0T82Cl9uAMSLEKhyE2zigwmmiQ7iU"
 TIMEZONE = "Europe/Moscow"
 
 # Для телеграм группы
@@ -9,3 +9,9 @@ GROUP_GUILD = "DarkSyndicate"  # Гильдия для групповых уве
 
 # Ссылка на таблицу гугл с боссами
 SpreadSheet_URL = "https://docs.google.com/spreadsheets/d/1FkMJd5Oj1MFy4TQZm379PDT3wu0gAqaxdgUgvRr9e-c/edit?gid=275501199#gid=275501199"
+
+#Для бота дс
+DISCORD_TOKEN = "MTQzNzQzMzM2MTA0NzA5NzQyNg.GDjxgU.-ejw3d4aN-wSsfz0p0G79Jrwk3rKDfQFTqeDeQ"
+
+# ID роли Raven2 для упоминаний
+DISCORD_ROLE_ID = "1426622831692877864"
